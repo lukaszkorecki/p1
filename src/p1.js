53f3a7110c23f284710a4844e43bcc1fa14741ff
Łukasz Korecki <lukasz@coffeesounds.com>
2012-08-27 21:05:54 +0100

    wrapper script for interacting with the user, some comments and removal of hardcoded password path

--- src/p1.js
+++ src/p1.js
@@ -1,27 +1,30 @@
 var page = require('webpage').create(),
-    file = phantom.args[0],
+    pass_file = phantom.args[0],
+    file = phantom.args[1],
+    // hardcoded for now
     address = '/Users/lukasz/Dropbox/1p/1password.agilekeychain/1Password.html';
 
 page.viewportSize = { width: 600, height: 600 };
-page.onConsoleMessage = function (msg) { console.log('[1p] ' + msg);
+// console running in phantom/cli context
+page.onConsoleMessage = function (msg) { console.log('[p1] ' + msg);
 };
 
+// console running in browser context
 page.onError = function (msg, trace) {
   console.log(msg);
   trace.forEach(function(item) { console.log('[1p] [err]  ', item.file, ':', item.line); });
 };
 
+// woop
 page.open(address, function (status) {
-
   if (status !== 'success') {
+    // FIXME better debug
     console.log('Unable to load the address!');
+    phantom.exit(1);
   } else {
-    console.log('im here!');
-    // really unsafe: pass.js looks like this:
-    // PASSWORD = '<your 1password here>'
-    // but well...
-    page.injectJs('pass.js');
+    page.injectJs(pass_file);
     window.setTimeout(function () {
+      // TODO this needs to be extracted and modularized or something
       page.evaluate(function(){
         console.log('woop!');
         try {
@@ -31,7 +34,6 @@ page.open(address, function (status) {
           var s = document.getElementById('unlock');
           console.log(s);
 
-          console.log(PASSWORD);
           i.setAttribute('value', PASSWORD);
           s.click();
         }catch(e) {
@@ -41,6 +43,7 @@ page.open(address, function (status) {
 
 
     }, 1400);
+    // FIXME this needs to be in debug only mode?
     window.setTimeout(function(){
       // we want some proof, right?
       page.render(file);
