91a19261b91682d5fe16b3ac1e0959dd199c4512
Łukasz Korecki <lukasz@coffeesounds.com>
2012-11-18 12:32:32 +0000

    Make location of keychain file configurable

--- src/p1.js
+++ src/p1.js
@@ -1,11 +1,10 @@
 var page = require('webpage').create(),
-    pass_file = phantom.args[0],
-    file = phantom.args[1],
-    // hardcoded for now
-    address = '/Users/lukasz/Dropbox/1p/1password.agilekeychain/1Password.html';
+pass_file = phantom.args[0],
+keychain = phantom.args[1]+'/1password.agilekeychain/1Password.html',
+file = phantom.args[2];
 
-page.viewportSize = { width: 600, height: 600 };
-// console running in phantom/cli context
+// probably doesn't need to be bigger
+page.viewportSize = { width: 1000, height: 600 };
 page.onConsoleMessage = function (msg) { console.log('[p1] ' + msg);
 };
 
@@ -15,11 +14,12 @@ page.onError = function (msg, trace) {
   trace.forEach(function(item) { console.log('[1p] [err]  ', item.file, ':', item.line); });
 };
 
-// woop
-page.open(address, function (status) {
+// lets do this
+page.open(keychain, function (status) {
   if (status !== 'success') {
     // FIXME better debug
     console.log('Unable to load the address!');
+    console.log('Tried: '+keychain);
     phantom.exit(1);
   } else {
     page.injectJs(pass_file);
